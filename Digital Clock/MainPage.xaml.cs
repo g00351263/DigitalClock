8082078b49c17e3431a7b4ecdab7d1dc0b878ef4
raja <g00351263@gmit.ie>
2017-12-19 09:10:25 +0000

    New Push for full Directory

---
+++ Digital Clock/MainPage.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.Storage;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Digital_Clock
+{
+    
+
+    public sealed partial class MainPage : Page
+    {
+        bool black,white,red = false;
+        DispatcherTimer timer = new DispatcherTimer(); //library pre-made functions for time and date
+        public MainPage()
+        {      
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(1);// set time to seconds
+            timer.Tick += Timer_Tick; // call timer tick method for other detaisl
+            timer.Start(); // timer start at app start  
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            String month;
+            String realMonth;
+            
+            //current seconds//
+            seconds.Text = (DateTime.Now.Second).ToString();
+            
+            //current minutes//
+            minutes.Text = (DateTime.Now.Minute).ToString();
+            
+            //current hours//
+            hours.Text = (DateTime.Now.Hour).ToString();
+            
+            //current day//
+            txtDay.Text = (DateTime.Now.DayOfWeek).ToString();
+            
+            //current month//
+            month = (DateTime.Now.Month).ToString();
+            
+            //Number to Month Mapping//
+            if (month == "1")
+            {
+                realMonth = "JAN";
+            }
+            if (month == "2")
+            {
+                realMonth = "FEB";
+            }
+            if (month == "3")
+            {
+                realMonth = "MAR";
+            }
+            if (month == "4")
+            {
+                realMonth = "APR";
+            }
+            if (month == "5")
+            {
+                realMonth = "MAY";
+            }
+            if (month == "6")
+            {
+                realMonth = "JUN";
+            }
+            if (month == "7")
+            {
+                realMonth = "JUL";
+            }
+            if (month == "8")
+            {
+                realMonth = "AUG";
+            }
+            if (month == "9")
+            {
+                realMonth = "SEP";
+            }
+            if (month == "10")
+            {
+                realMonth = "OCT";
+            }
+            if (month == "11")
+            {
+                realMonth = "NOV";
+                Month.Text = realMonth.ToString();
+            }
+            if (month == "12")
+            {
+                realMonth = "DEC";
+                Month.Text = realMonth.ToString();
+            }
+
+            //getting day and year current//
+            Day.Text = (DateTime.Now.Day).ToString();
+            Year.Text = (DateTime.Now.Year).ToString();
+        }
+
+        private void stopWatchNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(StopWatch));  
+        }
+
+        //BackGround Colour Chooser Event
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton current = (RadioButton)sender;
+            
+            if (sender == radioButtonBlack)
+            {
+                white = false;
+                red = false;
+                black = radioButtonBlack.IsEnabled = true;
+                radioBackground();
+                radioButtonBlack.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+
+                backGrid.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+            else if (sender == radioButtonRed)
+            {
+                black = false;
+                white = false;
+                red = radioButtonRed.IsEnabled = true;
+                radioBackground();
+                radioButtonRed.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+
+                backGrid.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+            else if (sender == radioButtonWhite)
+            {
+                black = false;
+                red = false;
+                white = radioButtonWhite.IsEnabled = true;
+                radioBackground();
+                radioButtonWhite.Background = new SolidColorBrush(Windows.UI.Colors.White);
+
+                backGrid.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            }
+        }
+
+        private void radioBackground()
+        {
+            if(black == true)
+            {
+                radioButtonRed.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+                radioButtonWhite.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+            else if (white == true)
+            {
+                radioButtonRed.Background = new SolidColorBrush(Windows.UI.Colors.White);
+                radioButtonBlack.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            }
+            else if (red == true)
+            {
+                radioButtonWhite.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                radioButtonBlack.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+        }
+    }
+}
---
+++ Digital Clock/StopWatch.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using System.Threading;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Digital_Clock
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StopWatch : Page
+    {
+
+        DispatcherTimer timer = new DispatcherTimer();
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+   
+
+        bool black, white, red = false;
+       
+
+        public StopWatch() { 
+            this.InitializeComponent();
+        }
+
+        #region Timer Methods
+
+        private void StartTimer()
+        {
+            this.timer.Interval = new TimeSpan(0, 0, 1);
+            this.timer.Tick += timer_Tick;
+            this.timer.Start();
+        }
+
+        private void StopTimer()
+        {
+            this.timer.Tick -= timer_Tick;
+            this.timer.Stop();
+        }
+
+        private void ResetTimer()
+        {
+            this.minutes = this.seconds = this.hours = 0;
+            this.txtBox.Text = "0 : 0 : 0";
+        }
+
+        #endregion
+        private void timer_Tick(object sender, object e)
+        {
+            //Just change the text  here
+
+            ++this.seconds;
+            if (this.seconds == 60)
+            {
+                this.minutes++;
+                this.seconds = 0;
+            }
+            if (this.minutes == 60)
+            {
+                this.hours++;
+                this.minutes = 0;
+            }
+
+            this.txtBox.Text = string.Format("{0} : {1} : {2}", hours, minutes, seconds);
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!this.timer.IsEnabled)
+            {
+                this.StartTimer();
+            }
+        }
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.StopTimer();
+            this.ResetTimer();
+        }
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.timer.IsEnabled)
+            {
+                this.StopTimer();
+            }
+        }
+
+        private void stopWatchNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton current = (RadioButton)sender;
+
+            if (sender == radioButtonBlack)
+            {
+                white = false;
+                red = false;
+                black = radioButtonBlack.IsEnabled = true;
+                radioBackground();
+                radioButtonBlack.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+
+                backGrid.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+            else if (sender == radioButtonRed)
+            {
+                black = false;
+                white = false;
+                red = radioButtonRed.IsEnabled = true;
+                radioBackground();
+                radioButtonRed.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+
+                backGrid.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+            else if (sender == radioButtonWhite)
+            {
+                black = false;
+                red = false;
+                white = radioButtonWhite.IsEnabled = true;
+                radioBackground();
+                radioButtonWhite.Background = new SolidColorBrush(Windows.UI.Colors.White);
+
+                backGrid.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            }
+        }
+
+        private void radioBackground()
+        {
+            if (black == true)
+            {
+                radioButtonRed.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+                radioButtonWhite.Background = new SolidColorBrush(Windows.UI.Colors.Black);
+            }
+            else if (white == true)
+            {
+                radioButtonRed.Background = new SolidColorBrush(Windows.UI.Colors.White);
+                radioButtonBlack.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            }
+            else if (red == true)
+            {
+                radioButtonWhite.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                radioButtonBlack.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+        }
+    }
+}
